3620a96a76bb6f509e86a507b7d431a5a0d4e197
hlibbc <hlib.master@gmail.com>
2025-06-18 18:09:03 +0900

    “eip-20”, “metamask-interaction” working draft2

--- projects/metamask-interaction/hardhat.config.js
+++ projects/metamask-interaction/hardhat.config.js
@@ -16,7 +16,8 @@ module.exports = {
   // defaultNetwork: "development", // --network 생략하려면 필요. but 무조건 외부 provider 띄워야 함
   networks: {
     development: {
-      url: `http://127.0.0.1:8545`
+      url: `http://127.0.0.1:8545`,
+      accounts: [process.env.PRIVATE_KEY]
     }
   }
 };

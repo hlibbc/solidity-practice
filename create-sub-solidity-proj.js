7f1c49578a4a1394f1669deff18112ded3cea94a
hlibbc <hlib.master@gmail.com>
2025-06-22 20:47:31 +0900

    Write eip-20 token deployment code & test code

--- create-sub-solidity-proj.js
+++ create-sub-solidity-proj.js
@@ -78,6 +78,10 @@ const configContent = `
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: require("path").resolve(__dirname, "../../.env") });
 
+// 필독: 외부 RPC에 연결해서 deploy할 경우, 해당 노드들이 cancun을 지원하지 않을 수 있음
+// 이 경우, RPC 사양을 확인해서 맞는 버전으로 재컴파일 요망
+// hardhat node의 경우, 2.22.0 이상으로 설치해야 cancun 지원함
+// 잘못된 예시: hardhat version 2.13.0 (2022년 10월 릴리스: paris 일부 반영된 버전) -> cancun으로 컴파일한 후 올리면 에러난다.
 module.exports = {
   solidity: {
     version: "0.8.28",
--- projects/eip-20/scripts/deploy.js
+++ projects/eip-20/scripts/deploy.js
@@ -1,7 +1,12 @@
+const hre = require("hardhat");
+const { ethers } = hre;
+
 async function main() {
     const MyToken = await ethers.getContractFactory("MyToken");
-    const myToken = await MyToken.deploy(); // signer as verifier for demo
+    const myToken = await MyToken.deploy(); 
+
     console.log("Deploying ERC20 tx hash:", myToken.deploymentTransaction().hash);
+
     await myToken.waitForDeployment();
     console.log("ERC20 deployed to:", await myToken.getAddress());
 }

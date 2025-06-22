b46d7b6b431f7d809aae0500fc8ebdf384ab50f2
hlibbc <hlib.master@gmail.com>
2025-06-22 21:40:27 +0900

    “metamask-interaction” in progress

--- projects/metamask-interaction/hardhat.config.js
+++ projects/metamask-interaction/hardhat.config.js
@@ -1,6 +1,6 @@
 
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config({ path: require("path").resolve(__dirname, "../../.env") });
+require("dotenv").config({ path: require("path").resolve(__dirname, "./.env") });
 
 module.exports = {
   solidity: {

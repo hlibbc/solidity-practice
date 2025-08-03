7a561a9541bdff684de6b4fe2ddf4fdab8a74256
hlibbc <hlib.master@gmail.com>
2025-08-03 20:39:46 +0900

    update comments in StorageLayout prj

--- projects/storage-layout/scripts/deploy.js
+++ projects/storage-layout/scripts/deploy.js
@@ -1,6 +1,17 @@
+/**
+ * @file deploy.js
+ * @notice StorageLayoutExplanation 컨트랙트 배포 스크립트
+ * @author hlibbc
+ */
 const hre = require("hardhat");
 const { ethers } = hre;
 
+/**
+ * @notice StorageLayoutExplanation 컨트랙트를 배포한다.
+ * @dev ethers v6 버전 사용
+ *      - 스토리지 레이아웃을 설명하는 교육용 컨트랙트 배포
+ *      - 다양한 데이터 타입들의 스토리지 저장 방식을 시연
+ */
 async function main() {
     const StorageLayoutExplanation = await ethers.getContractFactory("StorageLayoutExplanation");
     const storageLayoutExplanation = await StorageLayoutExplanation.deploy(); // signer as verifier for demo
